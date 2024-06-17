9c848cf39aea9e709a2cf823981eed8cae19ebe0
artur-kushner <kushnerartur@gmail.com>
2024-06-17 17:35:14 +0100

    fix: comment

--- js/main.js
+++ js/main.js
@@ -1,11 +1,39 @@
 
-// SLIDER NAVIGATION ACTIVE
+// SLIDER NAVIGATION ACTIVE and  SLIDER 
 
 document.addEventListener("DOMContentLoaded", function() {
     let currentIndex = 0;
     const slides = document.querySelectorAll('.slide-item');
     const dots = document.querySelectorAll('.nav-dot');
     const totalSlides = slides.length;
+
+    const backgrounds = [
+        "../../imgages/slider/Slide Image-1.jpg",
+        "../..//imgages/slider/Slide Image-2.jpg",
+        "../..//imgages/slider/Slide Image-3.jpg",
+        "../..//imgages/slider/Slide Image-4.jpg",
+        "../..//imgages/slider/Slide Image-5.jpg"
+    ];
+
+    function updateSlides() {
+        slides.forEach((slide, i) => {
+            slide.style.backgroundImage = `url('${backgrounds[(currentIndex + i) % totalSlides]}')`;
+            slide.style.zIndex = 1;
+            slide.classList.remove('slide-1', 'slide-2', 'slide-3', 'slide-4', 'slide-5');
+        });
+
+        slides[0].classList.add('slide-1');
+        slides[1].classList.add('slide-2');
+        slides[2].classList.add('slide-3');
+        slides[3].classList.add('slide-4');
+        slides[4].classList.add('slide-5');
+
+        slides[0].style.zIndex = 5;
+        slides[1].style.zIndex = 4;
+        slides[2].style.zIndex = 3;
+        slides[3].style.zIndex = 2;
+        slides[4].style.zIndex = 1;
+    }
 
     function updateActiveDot(index) {
         dots.forEach((dot, i) => {
@@ -14,23 +42,29 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     document.querySelector('.nav-arrow-right').addEventListener('click', function() {
-        slides[currentIndex].classList.remove('active');
         currentIndex = (currentIndex + 1) % totalSlides;
-        slides[currentIndex].classList.add('active');
+        updateSlides();
         updateActiveDot(currentIndex);
     });
 
     document.querySelector('.nav-arrow-left').addEventListener('click', function() {
-        slides[currentIndex].classList.remove('active');
         currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
-        slides[currentIndex].classList.add('active');
+        updateSlides();
         updateActiveDot(currentIndex);
     });
 
-   
-    slides[currentIndex].classList.add('active');
+    dots.forEach((dot, index) => {
+        dot.addEventListener('click', function() {
+            currentIndex = index;
+            updateSlides();
+            updateActiveDot(currentIndex);
+        });
+    });
+
+    updateSlides();
     updateActiveDot(currentIndex);
 });
+
 
 // NEXT
 // HEADER ALL SECTION SHOW
@@ -56,3 +90,5 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
   
+
+
